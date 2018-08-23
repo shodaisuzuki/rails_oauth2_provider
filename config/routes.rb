865fe52cb1435b971dfafe877dacff763b9c2305
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-08-23 16:46:34 +0900

    root_pathを指定しないとエラーになるので、doorkeeper/applications#indexをrootに指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   use_doorkeeper
   devise_for :users
+
+  root 'doorkeeper/applications#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
