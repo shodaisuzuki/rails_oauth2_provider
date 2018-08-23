2ae00434225ab026eddbcf578a9a1e0ea0f54615
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-08-23 17:13:03 +0900

    routes added for loginAPI

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'doorkeeper/applications#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  defaults format: :json do
+    namespace :api do
+      post 'login', to: 'credentials#login'
+    end
+  end 
 end
