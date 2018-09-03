7fa894269391e037ca3d31c13595c34ed66621fd
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-09-03 10:42:37 +0900

    loginAPI実装

--- app/controllers/api/credentials_controller.rb
+++ app/controllers/api/credentials_controller.rb
@@ -1,4 +1,10 @@
 module Api
   class CredentialsController < ApiController
+    # loginメソッドを外部から呼び出す際にdoorkeeperで認証処理する
+    before_action :doorkeeper_authorize!
+
+    def login
+      render json: { user: current_resource_owner }
+    end
   end
 end
