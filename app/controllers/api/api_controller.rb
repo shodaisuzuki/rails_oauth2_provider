5d3a9ffc6da151bc4cf8113d7385d54c786156e3
soartec-lab <shodai-connect@tbz.t-com.ne.jp>
2018-09-03 10:26:46 +0900

    リスクエストdoorkeeper_tokenからUser情報を取得し返す

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,13 @@
 module Api
   class ApiController < ::ApplicationController
+    # ApplicationControllerでauthenticate_userを呼び出している場合、
+    # ここでも処理がはしり、401エラーの元なので、切っておく
+    before_action :authenticate_user!, only: []
+
+   private
+
+   def current_resource_owner
+     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+   end
   end
 end
